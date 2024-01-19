a6c02cb6806dae692835296c8ba865f1791d34ab
Gabriel Ong <gbo1@sfu.ca>
2024-01-19 13:03:36 -0800

    Fixed some formatting for history command

--- Helper.py
+++ Helper.py
@@ -2,6 +2,19 @@ import requests
 import json
 from api import apiKey
 
+def show_table(table):
+    new_string = '```'
+
+    widths = [max([len(col) for col in cols]) for cols in zip(*table)]
+
+    for i in table:
+        for j in range(len(i)):
+            new_string += '| {:{}} '.format(i[j], widths[j])
+        new_string += '|\n'
+
+    new_string=new_string+"```"
+    return new_string
+
 def getPuuidFromUser(region, user, tag):
     getPuuid=requests.get("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+user+"/"+tag+"?api_key="+apiKey+"")
     accountInfo = getPuuid.json()
@@ -12,10 +25,6 @@ def getPuuidFromUser(region, user, tag):
 def getMatches(puuid):
     getMatches=requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=20&api_key="+apiKey+"")
     matchList=getMatches.json()
-    a=1
-    for i in matchList:
-        print(str(a)+" "+i)
-        a=a+1
     return matchList
 
 def getKDA(participant) ->str:
@@ -24,19 +33,24 @@ def getKDA(participant) ->str:
      str(participant['deaths']))
 
 def printMatchList(matchList, puuid) ->str:
-    ret=""
+    
     a=1
+    element=[[""]*3 for _ in range(len(matchList))]
     for id in matchList:
+        ret=""
         seeMatch =requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/"+str(id)+"?api_key="+apiKey+"")
         match = seeMatch.json()
         participantList=match['info']['participants']
-        ret=ret+"Match "+str(a)+":\n"
-        for i in participantList:
-            if(i['puuid']==puuid):
-                ret= ret + (i['championName']+'\n   '+
-                    getKDA(i)+'\n')
+        adding=str("Match "+str(a))
+        element[a-1][0]=adding
+        for i in range(len(matchList)):
+            if(participantList[i]['puuid']==puuid):
+                element[a-1][1]=str(participantList[i]['championName'])
+                element[a-1][2]=str(getKDA(participantList[i]))
+                break
+        
         a+=1
-    print(ret)
+    ret= show_table(element)
     return ret
         
 def getMatchInfo(matchId):
@@ -50,13 +64,11 @@ def getMatchInfo(matchId):
     
     ret=''
     
-    for i in range(5):
-        print('here2')
-        ret=ret+str(participantList[i]['summonerName']+"\t "+participantList[i+5]['summonerName']+"\n    "+
-              participantList[i]['championName']+"\t"+participantList[i+5]['championName']+"\n    "+
+    for i in range(len(participantList)/2):
+        ret=ret+str(participantList[i]['summonerName']+"\t "+participantList[i+len(participantList)/2]['summonerName']+"\n    "+
+              participantList[i]['championName']+"\t"+participantList[i+len(participantList)/2]['championName']+"\n    "+
               getKDA(participantList[i])+
-              "\t"+getKDA(participantList[i+5])+"\n")
-    
-    print("meow"+ret)
+              "\t"+getKDA(participantList[i+len(participantList)/2])+"\n")
+
     return ret
     
